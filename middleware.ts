b6a669261916a741e3f1aef9bd8af989688233e4
Leo Guinan <leo.guinan@gmail.com>
2024-04-02 09:36:51 -0400

    making policy and terms page not gated by login

--- middleware.ts
+++ middleware.ts
@@ -28,5 +28,5 @@ export default auth((req) => {
 })
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|privacy|terms).*)']
 }
