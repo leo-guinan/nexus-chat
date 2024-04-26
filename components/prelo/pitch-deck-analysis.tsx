711ffe8a3afb8471f52d66af38e35748c1e7ac97
Leo Guinan <leo.guinan@gmail.com>
2024-04-26 11:02:32 -0400

    displaying markdown for report and centering

--- components/prelo/pitch-deck-analysis.tsx
+++ components/prelo/pitch-deck-analysis.tsx
@@ -1,6 +1,10 @@
 'use client'
 import {useEffect, useRef, useState} from "react";
 import {ICloseEvent, IMessageEvent, w3cwebsocket as W3CWebSocket} from "websocket";
+import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
+import {CodeBlock} from "@/components/ui/codeblock";
+import {MemoizedReactMarkdown} from "@/components/markdown";
 
 interface PitchDeckAnalysisProps {
     pitchDeckAnalysis: string
@@ -50,11 +54,49 @@ export default function PitchDeckAnalysis({pitchDeckAnalysis, uuid}: PitchDeckAn
     }, [uuid])
     return (
         <div>
-            <h1>Pitch Deck Analysis</h1>
-            <div className="text-base leading-6 cursor-text select-all flex flex-row">
-                <pre className="whitespace-pre-wrap font-sans">
+            <div className="text-base leading-6 cursor-text select-all flex flex-row w-full">
+                <MemoizedReactMarkdown
+                    className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 mx-auto"
+                    remarkPlugins={[remarkGfm, remarkMath]}
+                    components={{
+                        p({children}) {
+                            return <p className="mb-2 last:mb-0">{children}</p>
+                        },
+                        code({node, inline, className, children, ...props}) {
+                            if (children.length) {
+                                if (children[0] == '▍') {
+                                    return (
+                                        <span className="mt-1 cursor-default animate-pulse">▍</span>
+                                    )
+                                }
+
+                                children[0] = (children[0] as string).replace('`▍`', '▍')
+                            }
+
+                            const match = /language-(\w+)/.exec(className || '')
+
+                            if (inline) {
+                                return (
+                                    <code className={className} {...props}>
+                                        {children}
+                                    </code>
+                                )
+                            }
+
+                            return (
+                                <CodeBlock
+                                    key={Math.random()}
+                                    language={(match && match[1]) || ''}
+                                    value={String(children).replace(/\n$/, '')}
+                                    {...props}
+                                />
+                            )
+                        }
+                    }}
+                >
                     {displayedReport}
-                </pre>
+                </MemoizedReactMarkdown>
+
             </div>
         </div>
     );
